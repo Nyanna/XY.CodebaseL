e54dca4b3bd2774015a928aab74e221cccdf552f
Xyan <Xyan@xyx-xyan>
2011-06-06 19:03:46 +0000

    copy of new classes

---
+++ net/xy/codebasel/config/EnvarRetriever.java
@@ -0,0 +1,16 @@
+package net.xy.codebasel.config;
+
+import net.xy.codebasel.config.Config.IConfigRetriever;
+
+/**
+ * retrieves config from local environment vars
+ * 
+ * @author xyan
+ * 
+ */
+public class EnvarRetriever implements IConfigRetriever {
+
+    public Object load(final String key) {
+        return System.getenv(key.startsWith("env:") ? key.substring(4) : key);
+    }
+}
---
+++ net/xy/codebasel/config/SystemPropertyRetriever.java
@@ -0,0 +1,16 @@
+package net.xy.codebasel.config;
+
+import net.xy.codebasel.config.Config.IConfigRetriever;
+
+/**
+ * retrieves system properties
+ * 
+ * @author xyan
+ * 
+ */
+public class SystemPropertyRetriever implements IConfigRetriever {
+
+    public Object load(final String key) {
+        return System.getProperty(key.startsWith("system:") ? key.substring(7) : key);
+    }
+}
