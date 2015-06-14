de5ab7ee5deb3e7c933ff955c68754b01034f4b2
Xyan <xyan@gmx.net>
2015-06-14 13:05:42 +0200

    -added netbeans support -reduced memory allocations

--- src/net/xy/codebase/mem/ConcurrentPool.java
+++ src/net/xy/codebase/mem/ConcurrentPool.java
@@ -1,14 +1,15 @@
 package net.xy.codebase.mem;
 
-import java.util.concurrent.ConcurrentLinkedQueue;
+import net.xy.codebase.collection.ArrayQueue;
 
 public abstract class ConcurrentPool<R> {
-	private final ConcurrentLinkedQueue<R> pool = new ConcurrentLinkedQueue<>();
+	private final ArrayQueue<Object> pool = new ArrayQueue<>(Object.class, 1024 * 10);
 
 	abstract protected R newObject();
 
 	public R obtain() {
-		final R res = pool.poll();
+		@SuppressWarnings("unchecked")
+		final R res = (R) pool.take();
 		return res != null ? res : newObject();
 	}
 
