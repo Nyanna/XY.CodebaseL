7dc589563ef9c44626c64b385deb431a01987bdb
Xyan <xyan@gmx.net>
2016-03-19 18:21:32 +0100

    -added persistence hooks for configs

--- src/net/xy/codebase/cfg/Config.java
+++ src/net/xy/codebase/cfg/Config.java
@@ -11,13 +11,36 @@ import java.util.Properties;
  * @author Xyan
  *
  */
-public class Config extends AbstractConfig<String, Object> {
+public class Config<Key, Value> extends AbstractConfig<Key, Value> {
 
 	/**
 	 * default based on HashMap
 	 */
 	public Config() {
-		super(new HashMap<String, Object>());
+		super(new HashMap<Key, Value>());
+	}
+
+	/**
+	 * default with given map
+	 */
+	public Config(final HashMap<Key, Value> cfg) {
+		super(cfg);
+	}
+
+	/**
+	 * default with given map & parent
+	 */
+	public Config(final HashMap<Key, Value> cfg, final AbstractConfig<Key, Value> parent) {
+		super(cfg);
+		setParent(parent);
+	}
+
+	/**
+	 * default with parent
+	 */
+	public Config(final AbstractConfig<Key, Value> parent) {
+		super(new HashMap<Key, Value>());
+		setParent(parent);
 	}
 
 	/**
@@ -26,12 +49,13 @@ public class Config extends AbstractConfig<String, Object> {
 	 * @param args
 	 * @return
 	 */
+	@SuppressWarnings("unchecked")
 	public boolean parse(final String[] args) {
 		for (final String arg : args) {
 			final String[] pair = arg.split("=", 2);
 			final String key = pair[0].trim();
 			final Object val = parser.string2type(pair[1].trim());
-			values.put(key, val);
+			values.put((Key) key, (Value) val);
 		}
 		return true;
 	}
@@ -42,11 +66,12 @@ public class Config extends AbstractConfig<String, Object> {
 	 * @param props
 	 * @return
 	 */
+	@SuppressWarnings("unchecked")
 	public boolean parse(final Properties props) {
 		for (final Entry<Object, Object> entry : props.entrySet()) {
 			final String key = ((String) entry.getKey()).trim();
 			final Object val = parser.string2type(((String) entry.getValue()).trim());
-			values.put(key, val);
+			values.put((Key) key, (Value) val);
 		}
 		return true;
 	}
---
+++ src/net/xy/codebase/cfg/PersistentConfig.java
@@ -0,0 +1,118 @@
+package net.xy.codebase.cfg;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+/**
+ * base config without real persistence
+ *
+ * @author Xyan
+ *
+ */
+public class PersistentConfig extends Config<String, Object> {
+
+	/**
+	 * default based on HashMap
+	 */
+	public PersistentConfig() {
+		super(new HashMap<String, Object>());
+	}
+
+	/**
+	 * default with given map
+	 */
+	public PersistentConfig(final HashMap<String, Object> cfg) {
+		super(cfg);
+	}
+
+	/**
+	 * default with given map & parent
+	 */
+	public PersistentConfig(final HashMap<String, Object> cfg, final AbstractConfig<String, Object> parent) {
+		super(cfg, parent);
+	}
+
+	/**
+	 * default with parent
+	 */
+	public PersistentConfig(final AbstractConfig<String, Object> parent) {
+		super(new HashMap<String, Object>(), parent);
+	}
+
+	/**
+	 * persists and changes the value
+	 *
+	 * @param key
+	 * @param val
+	 */
+	public void storeValue(final String key, final Object val) {
+		values.put(key, val);
+	}
+
+	/**
+	 * parses and adds values from an serialized HashMap
+	 *
+	 * @param cfgFile
+	 */
+	public void parse(final File cfgFile) {
+		if (!cfgFile.exists())
+			return;
+
+		ObjectInputStream ois = null;
+		try {
+			final FileInputStream fis = new FileInputStream(cfgFile);
+			final BufferedInputStream bis = new BufferedInputStream(fis);
+			ois = new ObjectInputStream(bis);
+			@SuppressWarnings("unchecked")
+			final HashMap<String, Object> cfg = (HashMap<String, Object>) ois.readObject();
+
+			for (final Entry<String, Object> entry : cfg.entrySet())
+				putValue(entry.getKey(), entry.getValue());
+		} catch (final Exception e) {
+			throw new IllegalStateException("Error deserializing", e);
+		} finally {
+			if (ois != null)
+				try {
+					ois.close();
+				} catch (final IOException e) {
+					throw new IllegalStateException("Error closing input", e);
+				}
+		}
+	}
+
+	/**
+	 * stores this config content, not the parent, to the given file
+	 *
+	 * @param cfgFile
+	 */
+	public void store(final File cfgFile) {
+
+		if (cfgFile.getParentFile() != null)
+			cfgFile.getParentFile().mkdirs();
+
+		ObjectOutputStream oos = null;
+		try {
+			final FileOutputStream fos = new FileOutputStream(cfgFile);
+			final BufferedOutputStream bos = new BufferedOutputStream(fos);
+			oos = new ObjectOutputStream(bos);
+			oos.writeObject(values);
+		} catch (final Exception e) {
+			throw new IllegalStateException("Error deserializing", e);
+		} finally {
+			if (oos != null)
+				try {
+					oos.close();
+				} catch (final IOException e) {
+					throw new IllegalStateException("Error closing input", e);
+				}
+		}
+	}
+}
