587b16443bf958d99c111c9d3cf4e2a5a595a2b6
Xyan <xyan@gmx.net>
2016-07-19 00:29:56 +0200

    -added error logging when queue gets full

--- src/net/xy/codebase/collection/ArrayQueueSet.java
+++ src/net/xy/codebase/collection/ArrayQueueSet.java
@@ -3,6 +3,9 @@ package net.xy.codebase.collection;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * arrayqueue extended by an set to update elements in queue and ensure only one
  * equal object is present.
@@ -12,8 +15,7 @@ import java.util.Map;
  * @param <E>
  */
 public class ArrayQueueSet<E> extends ArrayQueue<E> {
-	// private static final Logger LOG =
-	// LoggerFactory.getLogger(ArrayQueueSet.class);
+	private static final Logger LOG = LoggerFactory.getLogger(ArrayQueueSet.class);
 	/**
 	 * backed set
 	 */
@@ -33,8 +35,10 @@ public class ArrayQueueSet<E> extends ArrayQueue<E> {
 	public synchronized boolean add(final E elem) {
 		if (set.put(elem, elem) == null) {
 			final boolean added = super.add(elem);
-			if (!added)
+			if (!added) {
 				set.remove(elem);
+				LOG.error("Queue rejected addition cuz too full [" + elem + "][" + this + "]");
+			}
 			return added;
 		}
 		return true;
